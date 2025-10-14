509d22104d335483836a222ebf27874a93856fdd
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2025-10-14 17:41:53 -0600

    Inicio Header

--- Reflex/PortafolioConReflex/PortafolioConReflex/PortafolioConReflex.py
+++ Reflex/PortafolioConReflex/PortafolioConReflex/PortafolioConReflex.py
@@ -1,29 +1,20 @@
 import reflex as rx
 from rxconfig import config
+from PortafolioConReflex.views.header import header 
 
-
-class State(rx.State):
-    """The app state."""
+# class State(rx.State):
+"""The app state."""
 
 
 def index() -> rx.Component:
-    return rx.container(
-        rx.heading("Página Principal", size="7"),
-        # 👇 ¡Aquí está el enlace!
-        rx.link(
-            rx.button("Ir a Contacto"),
-            href="/contacto",  # Usamos la ruta que definimos arriba
-            is_external=False, # Importante: Indica que es un enlace interno
-        )
+    return rx.box(
+        header(),
+        width = "100%",
+
     )
-def contacto() -> rx.Component:
-    return rx.container(
-        rx.heading("¡Contáctame!", size="7"),
-        rx.text("Aquí estarán mis datos de contacto..."),
-        # ¡Ojo! También agregamos un enlace de vuelta a la página principal
-        rx.link("Volver al inicio", href="/"),
-    )
+
+
 
 app = rx.App()
 app.add_page(index)
-app.add_page(contacto)
+
---
+++ Reflex/PortafolioConReflex/PortafolioConReflex/views/header.py
@@ -0,0 +1,51 @@
+import reflex as rx
+
+def header() -> rx.Component:
+    return rx.vstack(
+        rx.flex(
+            rx.box(
+                rx.text(
+                    "Sebastian Rabadan",
+                    size="7"
+                        ),
+                rx.text(
+                    "Aute anim cillum fugiat sit Lorem qui culpa pariatur ullamco voluptate officia consectetur deserunt ut. Esse pariatur qui elit reprehenderit adipisicing id veniam ut dolor cupidatat adipisicing ex nostrud Lorem. Esse incididunt laborum consequat occaecat quis consectetur amet mollit magna nisi. Non proident nulla duis est ipsum tempor sunt duis labore aliquip est dolor. Labore aliqua nulla nisi culpa voluptate incididunt quis elit fugiat officia veniam.",
+                    padding_top = "10px",
+                    padding_bottom = "10px" 
+                        ),
+                direction = "column",
+                align= "center",
+                size = "5",
+                width = "80%"
+                ),
+                rx.avatar(
+                    fallback="SR", 
+                    radius= "full", 
+                    size = "9",
+                    justify = "end"
+                    ),
+
+            direction = rx.breakpoints(
+                initial = "column",
+                sm = "column",
+                lg = "row"   
+            ),
+
+            justify="between",
+            align="center",
+            # gap = "30px",
+            width="80%",
+            padding = "10px",
+            height = "fit-content"
+
+        ),
+
+
+
+
+        width="100vw",
+        min_height= "20em",
+        background="linear-gradient(45deg, #B4B4B4, #32FFE775)",
+        align = "center",
+        justify = "center"
+    )
